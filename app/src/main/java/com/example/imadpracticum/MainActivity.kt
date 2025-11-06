d0591196e8ffdfd7cbe57d64934657fbf9e0bc90
Liam-RC-Haw1w <ST10499386@rcconnect.edu.za>
2025-11-06 08:29:07 +0000

    Starting Imad project and enum classes

--- app/src/main/java/com/example/imadpracticum/MainActivity.kt
+++ app/src/main/java/com/example/imadpracticum/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.imadpracticum
 
+
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,39 +11,29 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.imadpracticum.ui.theme.ImadpracticumTheme
 
 class MainActivity : ComponentActivity() {
+
+    // Enum used to switch between app screens
+    enum class Screen {
+        SPLASH, MAIN, LIST
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             ImadpracticumTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ImadpracticumTheme {
-        Greeting("Android")
-    }
-}
